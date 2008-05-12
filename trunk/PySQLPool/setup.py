312699b7631484dd32e7659b9a644f8ddda5f481
nerdynick <nerdynick@b35552d4-024d-0410-ac83-f182d93fe744>
2008-05-12 19:04:44 +0000

    Folder/Package base layout

---
+++ trunk/PySQLPool/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='PySQLPool',
+      version='0.1',
+      description='Python MySQL Connection Pooling and MySQL Query management',
+      author='Nick Verbeck',
+      author_email='nick@skeletaldesign.com',
+      url='http://www.skeletaldesign.com',
+      packages=['PySQLPool'],
+     )
