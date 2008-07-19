d38133f6dc070449b8a5fd8183e3c6307b9dc981
nerdynick <nerdynick@b35552d4-024d-0410-ac83-f182d93fe744>
2008-07-19 19:44:22 +0000

    Moved CommitOnEnd out of Query Obj into Connection Object to allow for comiting on at end of every query for a connection. Added commitOnEnd to getNewConnection, getNewQuery function to allow you to create auto commits quicker.

--- trunk/PySQLPool/PySQLPool/__init__.py
+++ trunk/PySQLPool/PySQLPool/__init__.py
@@ -1,12 +1,12 @@
 import PySQLConnection
 
-def getNewConnection(host='localhost', username='root', password='', schema='', port=3306):
-    return PySQLConnection.PySQLConnection(host=host, username = username,  password = password, schema = schema, port = port)
+def getNewConnection(host='localhost', username='root', password='', schema='', port=3306, commitOnEnd = False):
+    return PySQLConnection.PySQLConnection(host=host, username = username,  password = password, schema = schema, port = port, commitOnEnd = commitOnEnd)
 
 import PySQLQuery
-def getNewQuery(connection = None, host='localhost', username='root', password='', schema='', port=3306):
+def getNewQuery(connection = None, host='localhost', username='root', password='', schema='', port=3306, commitOnEnd = False):
     if connection is None:
-        return PySQLQuery.PySQLQuery(getNewConnection(host=host, username = username,  password = password, schema = schema, port = port))
+        return PySQLQuery.PySQLQuery(getNewConnection(host=host, username = username,  password = password, schema = schema, port = port, commitOnEnd = commitOnEnd))
     else:
         return PySQLQuery.PySQLQuery(connection)
 
