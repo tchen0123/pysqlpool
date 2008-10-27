240555a19407498c7ce31b8b878f064a3fb238ac
nerdynick <nerdynick@b35552d4-024d-0410-ac83-f182d93fe744>
2008-10-27 05:51:56 +0000

    Added Version Info and Updated Setup.py info

--- trunk/PySQLPool/setup.py
+++ trunk/PySQLPool/setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import PySQLPool
 
 setup(name='PySQLPool',
-      version='0.3',
+      version=PySQLPool.__version__,
       description='Python MySQL Connection Pooling and MySQL Query management',
       author='Nick Verbeck',
       author_email='nick@skeletaldesign.com',
-      url='http://www.skeletaldesign.com',
+      url='http://code.google.com/p/pysqlpool/',
+      download_url="http://code.google.com/p/pysqlpool/downloads/list",
       packages=['PySQLPool'],
      )
